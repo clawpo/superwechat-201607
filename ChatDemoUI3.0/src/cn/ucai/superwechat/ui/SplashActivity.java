7adbe551eeb1dbdc816cd838cf5aa98947b2fbe4
耿小泰 <clawpo@foxmail.com>
2016-11-01 11:11:02 +0800

    修改闪屏

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -2,11 +2,9 @@ package cn.ucai.superwechat.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.animation.AlphaAnimation;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.hyphenate.chat.EMClient;
+
 import cn.ucai.superwechat.DemoHelper;
 import cn.ucai.superwechat.R;
 
@@ -22,14 +20,6 @@ public class SplashActivity extends BaseActivity {
 	protected void onCreate(Bundle arg0) {
 		setContentView(R.layout.em_activity_splash);
 		super.onCreate(arg0);
-
-		RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
-		TextView versionText = (TextView) findViewById(R.id.tv_version);
-
-		versionText.setText(getVersion());
-		AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
-		animation.setDuration(1500);
-		rootLayout.startAnimation(animation);
 	}
 
 	@Override

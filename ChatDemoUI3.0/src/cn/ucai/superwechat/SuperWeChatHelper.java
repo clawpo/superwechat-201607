368f015ca6e035c2e02216dcfe8bfc9dcc6f9ac9
耿小泰 <clawpo@foxmail.com>
2016-11-09 10:45:16 +0800

    删除联系人

--- ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -649,12 +649,13 @@ public class SuperWeChatHelper {
         }
 
         @Override
-        public void onContactDeleted(String username) {
+        public void onContactDeleted(final String username) {
             L.e(TAG,"MyContactListener,onContactDeleted...");
             Map<String, EaseUser> localUsers = SuperWeChatHelper.getInstance().getContactList();
             localUsers.remove(username);
             userDao.deleteContact(username);
             inviteMessgeDao.deleteMessage(username);
+            SuperWeChatHelper.getInstance().delAppContact(username);
 
             broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
         }
@@ -1352,5 +1353,13 @@ public class SuperWeChatHelper {
         ArrayList<User> mList = new ArrayList<User>();
         mList.addAll(appContactList.values());
         demoModel.saveAppContactList(mList);
+    }
+
+    /**
+     * save single contact
+     */
+    public void delAppContact(String username){
+        getAppContactList().remove(username);
+        demoModel.delAppContact(username);
     }
 }

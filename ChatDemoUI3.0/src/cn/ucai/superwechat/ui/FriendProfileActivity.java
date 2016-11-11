d973fddf9e3db24c1cad8043bd994310f4c706f2
耿小泰 <clawpo@foxmail.com>
2016-11-11 15:36:59 +0800

    优化好友资料界面,获取数据失败的时候。如果非好友,直接退出页面,是好友则显示之前的数据

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -72,8 +72,10 @@ public class FriendProfileActivity extends BaseActivity {
     }
 
     private void syncFail(){
-        MFGT.finish(this);
-        return;
+        if(!isFriend) {
+            MFGT.finish(this);
+            return;
+        }
     }
 
     private void syncUserInfo() {

9c0221ea1b674659b82dbfda6d950106039742a8
耿小泰 <clawpo@foxmail.com>
2016-11-11 11:06:46 +0800

    显示群头像

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/PublicGroupsActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/PublicGroupsActivity.java
@@ -26,6 +26,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -35,11 +36,13 @@ import android.widget.Toast;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMCursorResult;
 import com.hyphenate.chat.EMGroupInfo;
-import cn.ucai.superwechat.R;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.hyphenate.exceptions.HyphenateException;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import cn.ucai.superwechat.R;
 
 public class PublicGroupsActivity extends BaseActivity {
 	private ProgressBar pb;
@@ -167,9 +170,11 @@ public class PublicGroupsActivity extends BaseActivity {
 	private class GroupsAdapter extends ArrayAdapter<EMGroupInfo> {
 
 		private LayoutInflater inflater;
+        Context mContet;
 
 		public GroupsAdapter(Context context, int res, List<EMGroupInfo> groups) {
 			super(context, res, groups);
+            mContet = context;
 			this.inflater = LayoutInflater.from(context);
 		}
 
@@ -181,6 +186,8 @@ public class PublicGroupsActivity extends BaseActivity {
 
 			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position).getGroupName());
 
+            EaseUserUtils.setAppGroupAvatar(mContet,getItem(position).getGroupId(),
+                    (ImageView) convertView.findViewById(R.id.avatar));
 			return convertView;
 		}
 	}
